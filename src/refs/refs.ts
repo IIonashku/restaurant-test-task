9833d4461652486d0f5b1882b2f0bb551e952f13
IIonashku <ihor.ionashku@gmail.com>
2023-03-16 12:18:05 +0200

    Added refs

---
+++ src/refs/refs.ts
@@ -0,0 +1,8 @@
+export const refs = {
+  list: document.getElementById("itemList"),
+  allergenCheckboxes: document.getElementById("allergenCheckboxes"),
+  veganCheckbox: document.getElementById("veganCheckbox"),
+  vegetarianCheckbox: document.getElementById("vegetarianCheckbox"),
+  searchInput: document.getElementById("searchInput"),
+  errorBoundary: document.getElementById("errorBoundary"),
+};
